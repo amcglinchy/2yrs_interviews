31c8eb43da8c5e61b7c03cb8c95fcffd7de01dcd
Alexandra <asmcglinchy@gmail.com>
2023-12-15 23:03:32 -0500

    first viz

--- src/app.js
+++ src/app.js
@@ -1,45 +1,37 @@
 import * as d3 from "d3";
-import ScrollMagic from "scrollmagic";
-import { TweenMax, TimelineMax } from "gsap"; // Also works with TweenLite and TimelineLite
-import { ScrollMagicPluginGsap } from "scrollmagic-plugin-gsap";
-import scrollama from "scrollama"; // or...
+import { gsap } from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
 
+gsap.registerPlugin(ScrollTrigger);
 
-ScrollMagicPluginGsap(ScrollMagic, TweenMax, TimelineMax);
-
-const scroller = scrollama();
-const width = window.innerWidth * .7,
-    height = window.innerHeight * .8,
+const width = window.innerWidth * 0.7,
+    height = window.innerHeight * 0.8,
     m = { top: 20, bottom: 50, left: 40, right: 40 };
-const barHeight = 25
-// const height = Math.ceil((state.data.length + 0.1) * barHeight) + marginTop + marginBottom;
-let svg, xScale, yScale;
+const barHeight = 30; // Adjusted bar height
+let svg, xScale, yScale, rect1, rect2;
 let totalInterviews, uniquePersons, idsMoreThanOnce;
 
 /* APPLICATION STATE */
 let state = {
     data: [],
     // selectedParty: "All" // + YOUR INITIAL FILTER SELECTION
-  };
-  
-  /* LOAD DATA */
-  import("../data/all_interviews_capstone_final.json").then(raw_data => {
+};
+
+/* LOAD DATA */
+import("../data/all_interviews_capstone_final.json").then(raw_data => {
     // + SET YOUR DATA PATH
     console.log("data", raw_data);
-    // save our data to application state
+    // save our data to the application state
     state.data = raw_data;
     init();
-  });
+});
 
-
-
-  /* INITIALIZING FUNCTION */
-  // this will be run *one time* when the data finishes loading in
-  function init() {
+/* INITIALIZING FUNCTION */
+function init() {
     totalInterviews = state.data.length;
     uniquePersons = new Set(state.data.map(d => d.id)).size;
     const idCounts = {};
-  
+
     state.data.forEach(item => {
         const id = item.id;
         idCounts[id] = (idCounts[id] || 0) + 1;
@@ -47,70 +39,114 @@ let state = {
 
     idsMoreThanOnce = Object.values(idCounts).filter(count => count > 1).length;
 
-
     xScale = d3
-    .scaleLinear()
-    .domain([0, Math.max(totalInterviews, uniquePersons, idsMoreThanOnce)])
-    .range([m.left, width - m.right]);
+        .scaleLinear()
+        .domain([0, Math.max(totalInterviews, uniquePersons, idsMoreThanOnce)])
+        .range([m.left, width - m.right]);
 
     yScale = d3
         .scaleBand()
         .domain(['Total Interviews', 'Unique Persons', 'IDs More Than Once'])
-        .range([m.top, height - m.bottom]) 
+        .range([m.top, height - m.bottom])
         .padding(0.1);
 
     svg = d3
         .select("#container")
         .append("svg")
         .attr("width", width)
-        .attr("height", height);  
-      const xAxis = d3.axisBottom(xScale);
-      const yAxis = d3.axisLeft(yScale);
+        .attr("height", height)
+        .attr("overflow", "visible");
 
-        console.log(uniquePersons, totalInterviews, idsMoreThanOnce)
-  
-  
-    draw(); // calls the draw function
-  }
-  
-  /* DRAW FUNCTION */
-  // we call this every time there is an update to the data/state
-  function draw() {
+    // const xAxis = d3.axisBottom(xScale);
+    // const yAxis = d3.axisLeft(yScale);
 
-    const barHeight = 30;
-    const yOffset = 40; 
-
-    // svg.selectAll("rect")
-    //     .data([totalInterviews, uniquePersons, idsMoreThanOnce])
-    //     .join(
-    //         enter => enter.append("rect")
-    //             .attr('x', m.left)  // Start x at the left margin
-    //             .attr('y', (_, i) => m.top + i * (barHeight + yOffset))                 
-    //             .attr('width', d => xScale(d))
-    //             .attr('height', barHeight)
-    //             .attr('fill', (_, i) => i === 0 ? 'steelblue' : 'orange'),  // Different color for the second bar
-    //         // + HANDLE UPDATE SELECTION
-    //         update => update,
-    //         // + HANDLE EXIT SELECTION
-    //         exit => exit.remove()
-    //     );
-
-    svg.selectAll("rect")
+    rect1 = svg
+    .selectAll(".bar1")
     .data([totalInterviews])
     .enter()
     .append("rect")
-    .class(".bar1")
-    .attr('x', m.left)
-    .attr('y', m.top)
-    .attr('width', 0)
-    .attr('height', yScale.bandwidth())
-    .attr('fill', '#095256')
-    .attr('class', 'bar')
-    .transition()
-    .duration(300)
-    .attr("width", d=>xScale(d));
+    .attr("x", m.left)
+    .attr("y", m.top)
+    .attr("width", 0)
+    .attr("height", barHeight)
+    .attr("fill", "#1169e4")
+    .attr("class", "bar1");
+
+// Append the second bar to the SVG
+    rect2 = svg
+    .selectAll(".bar2")
+    .data([uniquePersons])
+    .enter()
+    .append("rect")
+    .attr("x", m.left)
+    .attr("y", m.top+(barHeight*2))
+    .attr("width", xScale(uniquePersons))
+    .attr("height", barHeight)
+    .attr("fill", "#ec2d93")
+    .attr("opacity", "0")
+    .attr("class", "bar2");
+
+    rect3 = svg
+    .selectAll(".bar3")
+    .data([uniquePersons])
+    .enter()
+    .append("rect")
+    .attr("x", m.left)
+    .attr("y", m.bottom)
+    .attr("width", xScale(idsMoreThanOnce))
+    .attr("height", barHeight)
+    .attr("fill", "#ec6f08")
+    .attr("opacity", "0")
+    .attr("class", "bar3");
+
+    countText = svg
+    .append("text")
+    .attr("x", xScale(totalInterviews)+10) // Adjust the x position as needed
+    .attr("y", m.top + barHeight / 2) // Adjust the y position as needed
+    .attr("dy", ".35em")
+    .text(0) // Initial count is 0
+    .attr("class", "count-text");
+
+    bar2text = svg
+    .append("text")
+    .attr("x", xScale(uniquePersons)+50)
+    .attr("y", m.top+barHeight*2.5)
+    .attr("dy", ".35em")
+    .text(uniquePersons + " individuals")
+    .attr("class", "bar2text")
+    .attr("opacity", "0");
+
+    bar3text = svg
+    .append("text")
+    .attr("x", xScale(idsMoreThanOnce)+50)
+    .attr("y", m.top+(barHeight*4.75))
+    .attr("dy", ".35em")
+    .text(idsMoreThanOnce + " persons interviewed more than once")
+    .attr("class", "bar3text")
+    .attr("opacity", "0");
 
 
+    draw(); // calls the draw function
 }
+
+/* DRAW FUNCTION */
+function draw() {
+    // Append the first bar to the SVG
+    gsap.timeline({
+        scrollTrigger: {
+            trigger: "#container",
+            start: "top center", // Adjust the start position as needed
+            end: "+=300", // Adjust the end position as needed
+            markers: true, // Set to true to show trigger markers
+        },
+    })
+    .add('start')
+    .to(".bar1", {width: xScale(totalInterviews)-m.right, duration: 5}, 'start')
+    .to(countText, {text: totalInterviews + " interviews", duration: 5}, 'start')
+    .to(".bar2", {opacity: 1, delay: 1, duration: .3}, ">")
+    .to(".bar2text", {opacity: 1, delay: 1, duration: .3}, 5)
+    .to(".bar3", {opacity: 1, y: m.top+(barHeight*2.5), duration: 1}, ">")
+    .to(".bar3text", {opacity: 1, delay: 1, duration: .3}, 6)
+    };
 
 
