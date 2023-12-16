7348777e304f9a788d93b7c6a38a2d4c57e9135d
Alexandra <asmcglinchy@gmail.com>
2023-12-15 23:03:10 -0500

    first viz

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ import * as d3 from "d3";
 import ScrollMagic from "scrollmagic";
 import { TweenMax, TimelineMax } from "gsap"; // Also works with TweenLite and TimelineLite
 import { ScrollMagicPluginGsap } from "scrollmagic-plugin-gsap";
+import scrollama from "scrollama"; // or...
+
 
 ScrollMagicPluginGsap(ScrollMagic, TweenMax, TimelineMax);
 
-
+const scroller = scrollama();
 const width = window.innerWidth * .7,
     height = window.innerHeight * .8,
     m = { top: 20, bottom: 50, left: 40, right: 40 };
@@ -92,58 +94,23 @@ let state = {
     //         // + HANDLE EXIT SELECTION
     //         exit => exit.remove()
     //     );
-    // Create the first bar initially
-    const controller = new ScrollMagic.Controller();
 
-    // Create the first bar
     svg.selectAll("rect")
-        .data([totalInterviews])  // Start with a height of 0
-        .enter()
-        .append("rect")
-        .attr('x', m.left)
-        .attr('y', m.top)
-        .attr('width', 0)
-        .attr('height', barHeight)
-        .attr('fill', '#095256')
-        .attr('class', 'bar')
-        .transition()
-        .duration(1000)
-        .attr("width", d => xScale(d));  // Add a class for easier selection
-    
-    // Create ScrollMagic scene for growing the first bar
-    // const growFirstBarScene = new ScrollMagic.Scene({
-    //     triggerElement: "#container",  // Adjust the trigger element as needed
-    //     duration: 500,  // Adjust the duration of the animation
-    //     offset: 200,  // Adjust the offset to control when the animation starts
-    // })
-    //     .setTween(TweenMax.to(".bar", 1, { width: xScale(totalInterviews) }))  // Animation to grow the bar
-    //     .addTo(controller);
-    
-    // // Create the second bar with initial height set to 0
-    // svg.selectAll("rect.second-bar")
-    //     .data([uniquePersons])  // Initial height is 0
-    //     .enter()
-    //     .append("rect")
-    //     .attr('x', m.left)
-    //     .attr('y', m.top + barHeight + yOffset)  // Position it below the first bar
-    //     .attr('width', 0)  // Initial width is 0
-    //     .attr('height', 0)  // Initial height is 0
-    //     .attr('fill', 'orange')
-    //     .attr('class', 'second-bar');  // Add a class for easier selection
+    .data([totalInterviews])
+    .enter()
+    .append("rect")
+    .class(".bar1")
+    .attr('x', m.left)
+    .attr('y', m.top)
+    .attr('width', 0)
+    .attr('height', yScale.bandwidth())
+    .attr('fill', '#095256')
+    .attr('class', 'bar')
+    .transition()
+    .duration(300)
+    .attr("width", d=>xScale(d));
 
-    // // Create ScrollMagic scene for growing the second bar
-    // const growSecondBarScene = new ScrollMagic.Scene({
-    //     triggerElement: "#container",
-    //     duration: 300,
-    //     offset: 400
-    // })
-    // .on("progress", (event) => {
-    //     // Animate the growth of the second bar based on the scroll progress
-    //     const newHeight = event.progress * barHeight;
-    //     svg.selectAll(".second-bar")
-    //         .attr('height', newHeight);
-    // })
-    // .addTo(controller);
+
 }
 
 
